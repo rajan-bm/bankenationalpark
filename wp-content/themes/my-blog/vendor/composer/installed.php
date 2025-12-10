951ba2f743887da1c9b94b5c83bfefa8cbb26771
razan home <rajan@cubit.com.np>
2025-12-10 23:00:56 +0545

    new cahnges

---
+++ wp-content/themes/my-blog/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'vendor/razan',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'd27009f68d86fc9f1e0a5a870a64710dc3c68b09',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'vendor/razan' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'd27009f68d86fc9f1e0a5a870a64710dc3c68b09',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
